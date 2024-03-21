f8e153c97ce2fb46585a96ab47cccbd44fa269a6
Alif Iqbal Abesh <alifcodes101@gmail.com>
2024-03-22 03:09:07 +0600

    added the week 1 assignment files and went through it

---
+++ assignments/week-1/offline-class-1/level-1/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+function findExample(arr) {
+  console.log("Original Array:", arr);
+
+  let found = arr.find(function(item) {
+    return item > 3;
+  });
+  console.log("After find:", found);
+}
+findExample([1, 2, 3, 4, 5]);
